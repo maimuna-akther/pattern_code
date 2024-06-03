8e6bc2ad5e7ba3d2e265d6d4c05c3fb083780e24
maimuna-akther <maimuna.csecu@gmail.com>
2024-06-03 21:30:20 +0600

    prog89_pattern.diff is added

---
+++ prog89_pattern_diff.c
@@ -0,0 +1,27 @@
+// Output : 
+// How many rows of right triangle?
+// 5               
+// * * * * *
+// * * * *
+// * * * 
+// * *
+// *
+   
+#include<stdio.h>
+int main()
+{       
+        int row, col, rows = 5;
+        printf(" How many rows of right triangle?\n %d\n", rows);
+        	printf(" \n ");
+
+	for (row = 5; row >= 1; row--)
+	{
+		for (col = 1; col <= row; col++)
+		{
+			printf(" * ");
+		}
+			printf(" \n ");
+	}
+	return 0;
+}
+        
